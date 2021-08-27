b777a7196fa78f4684d7bc9df5ee542b8d96e1f5
shotaro-ooe <shoooe97@gmail.com>
2021-08-27 23:40:01 +0900

    ブロックの生成成功

---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+//ブロック全体を管理するクラス
+//BlockObjは個々のブロックを管理するインナークラス
+public class Blocks {
+	//一つ一つのブロックの場所を保存するクラス
+	public class BlockObj
+	{
+		public BlockObj(int x, int z, GameObject b)
+		{
+			this.X = x;
+			this.Z = z;
+			this.Block = b;
+		}
+		public int X {get; private set;}
+		public int Z {get; private set;}
+		//その位置にブロックが存在するのか？
+		public GameObject Block {get; set; }
+	}
+
+	//これでブロックを作る
+	GameObject prefab;
+	//floorの情報の保持
+	Transform floor;
+	int width;
+	int height;
+	BlockObj[] blocks;
+	//必要に応じてブロックの情報をintの配列にコピーする
+	int[] map;
+	//コピーの必要があるかないか?
+	bool remap;
+	//一つ一つのブロックのサイズの保存
+	Vector3 blockSize;
+	//ブロックのセーブのkeyName
+	string prefsName;
+
+	//これらを保存するためのコンストラクタ
+	public Blocks(GameObject prefab,Transform floor, int dx, int dz, string prefsName)
+	{
+		this.prefab = prefab;
+		this.floor = floor;
+		this.width = dx;
+		this.height = dz;
+		this.prefsName = prefsName;
+		this.blockSize = prefab.GetComponent<Transform>().localScale;
+
+		//blocksの初期化
+		//10*10分作る
+		blocks = new BlockObj[width * height];
+		map = new int[blocks.Length];
+		//Selectを使うことで、indexをつけることができる。
+		foreach (var item in blocks.Select((v,i) => new {v,i}))
+		{
+			blocks[item.i] = new BlockObj(i2x(item.i),i2z(item.i),null);
+
+		}
+	}
+
+	public void Init(Dictionary<string,int[]> objPositions)
+	{
+
+	}
+
+	//インデックスから横と縦を計算する
+	//縦と横からインデックスを計算する
+	//いろんなパターンに合わせて関数を作る
+	public int i2x(int i)
+	{
+		return i % height;
+	}
+
+	public int i2z(int i)
+	{
+		return i / width;
+	}
+
+	public int[] i2xz(int i)
+	{
+		return new int[] { i2x(i), i2z(i) };
+	}
+	public int xz2i(int[] xz)
+	{
+		return xz2i(xz[0], xz[1]);
+	}
+	public int xz2i(int x,int z)
+	{
+		return x + z * width;
+	}
+
+	public BlockObj Find(GameObject obj)
+	{
+		//blocksの中のBlockがobjと一致しているかどうか、成功したらそのクラスを返す
+		return Array.Find<BlockObj>(blocks, x => x.Block == obj);
+	}
+
+	//3次元の位置からブロックのセルの位置を検索する関数
+	public int[] GetBlockIndexXZ(Vector3 pos)
+    {
+		//それぞれの数字を配列に格納する。
+        int[] index = new int[] {
+            Mathf.FloorToInt(( ( pos.x - (floor.position.x - floor.localScale.x/2f) ) * width / floor.localScale.x )),
+            Mathf.FloorToInt( ( pos.z - (floor.position.z - floor.localScale.z/2f) ) * height / floor.localScale.z ),
+        };
+        return index;
+    }
+    public int GetBlockIndex(Vector3 pos)
+    {
+        return xz2i(GetBlockIndexXZ(pos));
+    }
+	//indexのx,zを指定して、save(作られたブロックを保存するかどうか)
+	public void CreateBlock(int x, int z, bool save = true)
+    {
+		//blocksのindexのBlockにprefabをInstantiateする
+        blocks[xz2i(x, z)].Block = UnityEngine.Object.Instantiate(prefab, GetBlockPosition(x, z), Quaternion.identity);
+        remap = true;
+        if (save)
+        {
+            //SavePrefs();
+        }
+    }
+	
+	//ブロックのインデックスから３次元の座標を取得する
+	public Vector3 GetBlockPosition(int index)
+    {
+        return GetBlockPosition(i2x(index), i2z(index));
+    }
+    public Vector3 GetBlockPosition(int iX, int iZ)
+    {
+        return new Vector3(
+            iX * floor.localScale.x / width + (floor.position.x - floor.localScale.x / 2f) + blockSize.x / 2f,
+            floor.position.y + floor.localScale.y / 2f + blockSize.y / 2f,
+            iZ * floor.localScale.z / height + (floor.position.z - floor.localScale.z / 2f) + blockSize.z / 2f
+            );
+    }
+	public void RemoveBlock(int x, int z, bool save = true)
+    {
+        RemoveBlock(blocks[xz2i(x, z)], save);
+    }
+    public void RemoveBlock(BlockObj obj, bool save = true)
+    {
+        UnityEngine.Object.Destroy(obj.Block);
+        obj.Block = null;
+        remap = true;
+        if (save)
+        {
+            //SavePrefs();
+        }
+    }
+	
+}
---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Floor: MonoBehaviour {
+
+	public GameObject blockPreb;
+	public Blocks blocks;
+	int dx = 10;
+	int dz = 10;
+	//Transformの初期化
+	Transform floor;
+	
+	string playerName = "Player";
+	string enemyName = "Enemy";
+	string goalName = "Goal";
+	string startName = "Start";
+
+	//floorのポジションの配列
+	Dictionary<string,int[]> objPositions = new Dictionary<string,int[]>();
+	
+	// Use this for initialization
+	void Start () {
+		floor = GetComponent<Transform>();
+
+		// Object start position
+        objPositions[playerName] = new int[] { 0, 0 };
+        objPositions[startName] = new int[] { 0, 0 };
+        objPositions[goalName] = new int[] { dx - 1, dz -1 };
+        objPositions[enemyName] = new int[] { Mathf.RoundToInt(dx/2), Mathf.RoundToInt(dz/2) };
+		
+		//create blocks
+		//block size,instance
+		blockPreb.GetComponent<Transform>().localScale =
+            new Vector3(floor.localScale.x / dx, 1f, floor.localScale.z / dz);
+        blocks = new Blocks(blockPreb, floor,dx,dz,"map");
+		//どこにそれぞれがポジションしているか確認
+        blocks.Init(objPositions);
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//左右のマウスクリックを同時にチェックする
+		int i = Enumerable.Range(1, 2).FirstOrDefault(v => Input.GetMouseButtonDown(v - 1));
+		//どちらかのマウスがクリックされていたら？
+		if (i != 0)
+		{
+			/*
+			Rayがブロックに当たる場合は、そのブロックを削除する。
+			Rayが床に当たる場合は、その場所にブロックを作成する。
+			*/
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit = new RaycastHit();
+			
+			/*
+			ray.origin rayのスタート場所(カメラの場所)
+			ray.direction マウスのポジションの方向に向かっていく。
+			hit ここにどのオブジェクトがヒットしたのか？
+			Mathf.Infinity rayの長さで無限大の長さでrayを発射する。
+			*/
+			//rayが何かしらのオブジェクトに当たった場合
+			
+			if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+			{
+				//blocksの一覧に衝突したgameObjectがあるかどうか、そのブロックをtargetに保存
+				Blocks.BlockObj target = blocks.Find(hit.collider.gameObject);
+				//ブロックにぶつかり、右クリックの場合は削除
+				if (i == 2 && target != null)
+				{
+					blocks.RemoveBlock(target);
+				}
+				//floorのgameObjectと衝突したものが同じだった場合 
+				else if (i == 1 && gameObject == hit.collider.gameObject)
+				{
+					//hit.pointを変換して、そこにブロックを作る。
+					int[] index = blocks.GetBlockIndexXZ(hit.point);
+					blocks.CreateBlock(index[0],index[1]);
+				}
+				
+			}
+		}
+	}
+}
